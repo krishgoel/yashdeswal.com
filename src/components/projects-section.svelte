fa31221ab6f894c16d47c4396f6bea7d94127a57
Krish Goel <krishgoel3@gmail.com>
2020-12-18 19:17:49 +0530

    Icons -v2, added all projects

--- src/components/projects-section.svelte
+++ src/components/projects-section.svelte
@@ -11,7 +11,19 @@
                     <p class="bold">🚀 Concept Documentation</p>
                     <p>Eyena is an IoT based smart mirror that measures your body temperature upon detecting your presence without any physical contact. The project isn't econonmically feasible with the current technology, hence I had to postpone it indefinitely.</p>
                     <p>
-                        <a href="https://krishgoel.herokuapp.com/projects/eyena">Documentation</a>
+                        <a href="https://krishgoel.herokuapp.com/projects/eyena">📁 Documentation</a>
+                    </p>
+                    <br/>
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/projects/alexation.png">
+                <div class="width-restriction">
+                    <h2>Alexation</h2>
+                    <p class="bold">🏠 Home automation solution</p>
+                    <p>Not very original, but Krish and I worked on this project, it eliminates the need of a central Alexa hub by creating an ecosystem using NodeMCU. It brings down the cost of automation from $20 to $3 per device.</p>
+                    <p>
+                        <a href="https://github.com/KrishSkywalker/alexation">💻 Github Repository</a>
                     </p>
                     <br/>
                 </div>
@@ -25,8 +37,34 @@
                     <p class="bold">🚀 Concept Documentation</p>
                     <p>A subscription-based STEM Kits delivery service model that Kartik Gupta and I developed for an IT event.</p>
                     <p>
-                        <a href="https://krishgoel.herokuapp.com/projects/binarybox">Documentation</a>
+                        <a href="https://krishgoel.herokuapp.com/projects/binarybox">📁 Documentation</a>
                     </p>
+                    <br/>
+                </div>
+            </div>
+            <div class="card">
+                <!-- <img src="images/projects/eyena.png"> -->
+                <div class="width-restriction">
+                    <br>
+                    <h2>Arduino Drumpads</h2>
+                    <p class="bold">🥁 MIDI Based Drums</p>
+                    <p>We designed e-drums while eliminating the MIDI and laptop requirements that require close to none setup and make it more cost-effective</p>
+                    <p>
+                        <a href="https://github.com/KrishSkywalker/arduino-drum-pads">💻 Github Repository</a>
+                    </p>
+                    <br/>
+                </div>
+            </div>
+            <div class="card">
+                <!-- <img src="images/projects/eyena.png"> -->
+                <div class="width-restriction">
+                    <br>
+                    <h2>Inturn</h2>
+                    <p class="bold">💼 Student internship matchmaking platform</p>
+                    <p>We designed e-drums while eliminating the MIDI and laptop requirements that require close to none setup and make it more cost-effective</p>
+                    <!-- <p>
+                        <a href="">📁 Documentation</a>
+                    </p> -->
                     <br/>
                 </div>
             </div>
